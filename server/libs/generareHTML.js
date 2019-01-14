649b83737503a72f2f4f86e77d4d12a7092b5b25
Jeanette Skoog <jeanetteskoog@n134-p95.eduroam.kth.se>
2019-01-14 15:23:06 +0100

    Fix (hopfully) for empty pages

--- server/libs/generareHTML.js
+++ server/libs/generareHTML.js
@@ -83,10 +83,10 @@ module.exports = function (syllabusObject, semester, lang = 'sv'){
 function toHeaderAndText(header, text, styleClass){
     header = header.length > 0 ? ` <h3>${header}</h3>`: ''
     return(
-        `<div class="${styleClass}" >
+        `<span class="${styleClass}" >
           ${header}
           <p> ${text} </p> 
-        </div>`
+        </span>`
     )
 }
 
@@ -122,13 +122,12 @@ function isValidData(dataObject, language = 0){
 
 function topHtml(courseCode){
     const style = `<style>
-        body{ background-color:#ffffff; font-size:11px; margin-left:40px; margin-right:40px; line-height: 15px;}
+        body{ background-color:#ffffff; font-size:11px; margin-left:40px; margin-right:40px; line-height: 15px; overflow:hidden}
         #kth-logo{ height:80px; margin-left:15px; margin-bottom: 20px;}
-        p{font-style: oblique; margin-top: 10px;}
-        i, cite, em, dfn { font-weight:600; !important; }
+        i, cite, em, dfn { font-weight:600; }
         .pdfContainer{ max-width:540px; background-color:#ffffff;}
         .pdfFooterText{  background-color:#ffffff; font-size: 9px; margin-left:10px; margin-right:10px; margin-top:20px; border-top: 1px solid #ddd; color: #444;}
-        .pdfContent p{margin-bottom: 5px; page-break-inside: avoid;}
+        .pdfContent p{ page-break-inside: avoid;}
         .pdfContent h3{margin-top: 20px;}
         .pdfSection{page-break-inside: avoid;}
         .secondTitle{ margin-top: 15px; margin-bottom: 17px; color:#808080; font-size: 16px; border-bottom:1px solid #808080}
