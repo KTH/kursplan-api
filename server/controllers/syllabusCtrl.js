9d1b0ca647b1e283a1609e8fbfe67cdbf5f7f7b9
Jeanette Skoog <jeanetteskoog@n129-p226.eduroam.kth.se>
2019-05-10 09:54:34 +0200

    corr of language param to kopps

--- server/controllers/syllabusCtrl.js
+++ server/controllers/syllabusCtrl.js
@@ -10,11 +10,9 @@ const co = require('co')
 const { safeGet } = require('safe-utils')
 const generateHTML = require('../libs/generareHTML')
 
-
 module.exports = {
   getSyllabus: co.wrap(getSyllabus)
 }
-
 
 const config = require('../configuration').server
 const BasicAPI = require('kth-node-api-call').BasicAPI
@@ -28,47 +26,44 @@ let koppsApiInternal = new BasicAPI({
   defaultTimeout: config.kopps.defaultTimeout
 })
 
-
 function * getSyllabus (req, res, next) {
-
   const courseCode = req.params.courseCode
   const semester = req.params.semester
-  const language = req.params.language.length === 2 ? req.params.language : "sv"
-  
+  const language = req.params.language.length === 2 ? req.params.language : 'sv'
+
   try {
     let syllabus = {}
-    const syllabuskoppsAPI_course_tot = yield koppsApiInternal.getAsync(`syllabuses/${courseCode}/${semester}?lang=${language}`)
+    const syllabuskoppsAPI_course_tot = yield koppsApiInternal.getAsync(`syllabuses/${courseCode}/${semester}?l=${language}`)
     syllabus = syllabuskoppsAPI_course_tot.body
-    //console.log("syllabus",syllabus)
+    // console.log("syllabus",syllabus)
     const syllabusHTML = generateHTML(syllabus, semester, language)
 
     const pdfConfig = {
-      "format": "A4",        
-      "orientation": "portrait", 
-      "border": "0",           
-      paginationOffset: 1,       // Override the initial pagination number
-      "header": {
-        "height": "15mm",
-        "contents": ""
+      'format': 'A4',
+      'orientation': 'portrait',
+      'border': '0',
+      paginationOffset: 1, // Override the initial pagination number
+      'header': {
+        'height': '15mm',
+        'contents': ''
       },
-      "footer": {
-        "height": "27mm",
-        "contents": {
+      'footer': {
+        'height': '27mm',
+        'contents': {
           default: `<div class="row " style="max-width:520px;">
           <div class="pdfFooterText col-12" style="text-align: left;"> ${syllabusHTML.footerText}. 
-              <span class="" style="text-align:right; float:right"> ${language === 'en' ? "Page" : "Sida"} {{page}} ${language === 'en' ? "of  " : "av  " } {{pages}}</span></div> 
+              <span class="" style="text-align:right; float:right"> ${language === 'en' ? 'Page' : 'Sida'} {{page}} ${language === 'en' ? 'of  ' : 'av  '} {{pages}}</span></div> 
             </div> `
         }
       },
-      "zoomFactor": "1",
-      "base": "https://www.kth.se", 
-      "type": "pdf",
-      "phantomPath": "/usr/bin/phantomjs",
-      "timeout": 30000
+      'zoomFactor': '1',
+      'base': 'https://www.kth.se',
+      'type': 'pdf',
+      'phantomPath': '/usr/bin/phantomjs',
+      'timeout': 30000
     }
 
-    res.send({ syllabusHTML, pdfConfig})
-
+    res.send({ syllabusHTML, pdfConfig })
   } catch (err) {
     next(err)
   }
