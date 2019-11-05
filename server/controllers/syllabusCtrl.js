7990dff5b4e982778b913013711a952ed2704758
Elena Rakhimova <elenara@kth.se>
2019-11-05 11:45:32 +0100

    white

--- server/controllers/syllabusCtrl.js
+++ server/controllers/syllabusCtrl.js
@@ -44,7 +44,7 @@ function * getSyllabus (req, res, next) {
       paginationOffset: 1, // Override the initial pagination number
       'header': {
         'height': '15mm',
-        'contents': 'www.kth.se'
+        'contents': '<span style="color:white;display:none;">www.kth.se</span>'
       },
       'footer': {
         'height': '27mm',
