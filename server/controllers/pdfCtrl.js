1bcdf29141c80cb69813f2d203c86a89694c5e4a
Rickard Falk <rickard.falk@gmail.com>
2020-07-08 10:05:14 +0200

    Added support for syllabus PDF document name

--- server/controllers/pdfCtrl.js
+++ server/controllers/pdfCtrl.js
@@ -8,7 +8,8 @@ async function getSyllabus(req, res, next) {
   const { courseCode, semester } = req.params;
   const language =
     req.params.language.length === 2 ? req.params.language : "sv";
-  const { download } = req.query;
+  const { documentName, download } = req.query;
+  const fileName = documentName || `${courseCode}-${semester}`;
   log.debug(
     "getSyllabus: Received request for PDF with courseCode: ",
     courseCode,
@@ -19,7 +20,6 @@ async function getSyllabus(req, res, next) {
   );
   try {
     const contentDisposition = download === "true" ? "attachment" : "inline";
-    const fileName = `${courseCode}-${semester}`;
     res.type("application/pdf");
     res.set(
       "Content-Disposition",
