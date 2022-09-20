c571ea25d78076cecaad9627eda0d47705a398e4
Elena Rakhimova <elenara@kth.se>
2022-09-20 10:18:05 +0200

    fix (KUI-777): remove course_eligibility in case of a contract education

--- server/components/SyllabusPageFooter.jsx
+++ server/components/SyllabusPageFooter.jsx
@@ -1,42 +1,14 @@
-import React from "react";
-import { View, Text } from "@react-pdf/renderer";
+import React from 'react'
+import { View, Text } from '@react-pdf/renderer'
 
-import styles from "./SyllabusStyles";
-import i18n from "../../i18n";
-
-const getSelectedSyllabus = (
-  syllabusObject,
-  semester = "20101",
-  language = 0
-) => {
-  let count = 0;
-  let selectedSyllabus = {};
-  const syllabuses = syllabusObject.publicSyllabusVersions;
-
-  for (let i = 0; i < syllabuses.length; i++) {
-    if (Number(syllabuses[i].validFromTerm.term) === Number(semester)) {
-      selectedSyllabus.edition = syllabuses[i].edition;
-      selectedSyllabus.index = count;
-      selectedSyllabus.semesterNumber = syllabuses[i].validFromTerm.term
-        .toString()
-        .substring(4, 5);
-      selectedSyllabus.year = syllabuses[i].validFromTerm.term
-        .toString()
-        .substring(2, 4);
-    }
-    count++;
-  }
-  return selectedSyllabus;
-};
+import styles from './SyllabusStyles'
+import i18n from '../../i18n'
+import { getSelectedSyllabus } from './syllabusFilter'
 
 const footerText = (languageIndex, syllabus, semester) => {
-  const { courseCode } = syllabus.course;
-  const selectedSyllabus = getSelectedSyllabus(
-    syllabus,
-    semester,
-    languageIndex
-  );
-  const translation = i18n.messages[languageIndex].course_pdf_footer_string;
+  const { courseCode } = syllabus.course
+  const selectedSyllabus = getSelectedSyllabus(syllabus, semester, languageIndex)
+  const translation = i18n.messages[languageIndex].course_pdf_footer_string
   return (
     translation.for_code +
     courseCode +
@@ -45,28 +17,23 @@ const footerText = (languageIndex, syllabus, semester) => {
     selectedSyllabus.year +
     translation.edition +
     selectedSyllabus.edition
-  );
-};
+  )
+}
 
 const SyllabusPageFooter = ({ syllabus, semester, language }) => {
-  const languageIndex = language === "en" ? 0 : 1;
-  const { course } = syllabus;
+  const languageIndex = language === 'en' ? 0 : 1
   return (
     <View style={styles.pageFooter}>
-      <Text style={styles.pageFooterLeft}>
-        {footerText(languageIndex, syllabus, semester)}
-      </Text>
+      <Text style={styles.pageFooterLeft}>{footerText(languageIndex, syllabus, semester)}</Text>
       <Text
         style={styles.pageFooterRight}
         render={({ pageNumber, totalPages }) =>
-          languageIndex
-            ? `Sida ${pageNumber} av ${totalPages}`
-            : `Page ${pageNumber} of ${totalPages}`
+          languageIndex ? `Sida ${pageNumber} av ${totalPages}` : `Page ${pageNumber} of ${totalPages}`
         }
         fixed
       />
     </View>
-  );
-};
+  )
+}
 
-export default SyllabusPageFooter;
+export default SyllabusPageFooter
--- server/components/SyllabusPages.jsx
+++ server/components/SyllabusPages.jsx
@@ -1,76 +1,30 @@
-import React from "react";
-import { Page, View } from "@react-pdf/renderer";
+import React from 'react'
+import { Page, View } from '@react-pdf/renderer'
 
-import SyllabusHead from "./SyllabusHead";
-import SyllabusKeyInformation from "./SyllabusKeyInformation";
-import SyllabusBody from "./SyllabusBody";
-import SyllabusPageFooter from "./SyllabusPageFooter";
-import styles from "./SyllabusStyles";
-
-// Logic copied from generareHTML
-const getSelectedSyllabus = (
-  syllabusObject,
-  semester = "20101",
-  language = 0
-) => {
-  let count = 0;
-  let selectedSyllabus = {};
-  const syllabuses = syllabusObject.publicSyllabusVersions;
-
-  for (let i = 0; i < syllabuses.length; i++) {
-    if (Number(syllabuses[i].validFromTerm.term) === Number(semester)) {
-      selectedSyllabus.edition = syllabuses[i].edition;
-      selectedSyllabus.index = count;
-      selectedSyllabus.semesterNumber = syllabuses[i].validFromTerm.term
-        .toString()
-        .substring(4, 5);
-      selectedSyllabus.year = syllabuses[i].validFromTerm.term
-        .toString()
-        .substring(2, 4);
-    }
-    count++;
-  }
-  return selectedSyllabus;
-};
-
-const getActiveSyllabus = (syllabus, selectedSyllabus) => {
-  const activeSyllabus =
-    syllabus.publicSyllabusVersions[selectedSyllabus.index];
-  return activeSyllabus;
-};
+import SyllabusHead from './SyllabusHead'
+import SyllabusKeyInformation from './SyllabusKeyInformation'
+import SyllabusBody from './SyllabusBody'
+import SyllabusPageFooter from './SyllabusPageFooter'
+import styles from './SyllabusStyles'
+import { getActiveSyllabus, getSelectedSyllabus } from './syllabusFilter'
 
 /* A4 is default page size value, explicitly set for clarity */
 const SyllabusPages = ({ syllabus, semester, language }) => {
-  const selectedSyllabus = getSelectedSyllabus(syllabus, semester);
-  const activeSyllabus = getActiveSyllabus(syllabus, selectedSyllabus);
+  const selectedSyllabus = getSelectedSyllabus(syllabus, semester)
+  const activeSyllabus = getActiveSyllabus(syllabus, selectedSyllabus)
+
   return (
     <Page size="A4" style={styles.pages}>
       <View style={styles.content}>
-        <SyllabusHead
-          syllabus={syllabus}
-          activeSyllabus={activeSyllabus}
-          language={language}
-        />
-        <SyllabusKeyInformation
-          syllabus={syllabus}
-          activeSyllabus={activeSyllabus}
-          language={language}
-        />
-        <SyllabusBody
-          syllabus={syllabus}
-          activeSyllabus={activeSyllabus}
-          language={language}
-        />
+        <SyllabusHead syllabus={syllabus} activeSyllabus={activeSyllabus} language={language} />
+        <SyllabusKeyInformation syllabus={syllabus} activeSyllabus={activeSyllabus} language={language} />
+        <SyllabusBody syllabus={syllabus} activeSyllabus={activeSyllabus} language={language} />
       </View>
       <View fixed style={styles.footer}>
-        <SyllabusPageFooter
-          syllabus={syllabus}
-          semester={semester}
-          language={language}
-        />
+        <SyllabusPageFooter syllabus={syllabus} semester={semester} language={language} />
       </View>
     </Page>
-  );
-};
+  )
+}
 
-export default SyllabusPages;
+export default SyllabusPages
