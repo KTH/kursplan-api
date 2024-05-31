9c1e52c309b8ff8d394cb42d8fb41efe3df9e387
Benjamin Langlotz <benjala@kth.se>
2024-05-31 10:44:25 +0200

    chore(deps): remove local version of react-pdf
    and install npm version of react-pdf
    Overriding restructure was needed because of this issue: https://github.com/diegomura/react-pdf/issues/2675

--- lib/@react-pdf/renderer-2.0.0-alpha.15/types/image.d.ts
+++
@@ -1,6 +0,0 @@
-type HTTPMethod = 'GET' | 'HEAD' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
-
-export type SourceObject =
-  | string
-  | { data: Buffer; format: 'png' | 'jpg' }
-  | { uri: string; method: HTTPMethod; body: any; headers: any };
